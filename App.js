da3ab7045d6c546afbd1ccf6d40e5d83909a424f
Charlie Frye <klayton.alan@gmail.com>
2018-04-03 14:50:03 +0500

    test TextInput

--- App.js
+++ App.js
@@ -1,32 +1,20 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, StyleSheet, Text, View, TextInput } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  state = {
+    placeName: ''
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  placeNameChangedHandler = value => {
+    this.setState({placeName: value})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <TextInput value={this.state.placeName} onChangeText={this.placeNameChangedHandler}
+                   style={{width: 300, borderColor: 'black', borderWidth: 1}}/>
       </View>
     );
   }
@@ -43,10 +31,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
