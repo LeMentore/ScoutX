c494c8eb1b30f78ddf7380a8a917df158a75f984
Charlie Frye <32540979+LeMentore@users.noreply.github.com>
2018-04-07 22:18:23 +0500

    fix

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -31,7 +31,7 @@ const placesReducer = (state = initialState, action) => {
             return {
                 ...state,
                 selectedPlace: state.places.find(place => {
-                    return place.key === action.key
+                    return place.key === action.placeKey
                 })
             };
 
