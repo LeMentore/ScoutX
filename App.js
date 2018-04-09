1a2ea9ccbe325246a67b97e73c281b99a01771e6
Charlie Frye <klayton.alan@gmail.com>
2018-04-09 18:18:42 +0500

    adding a tabs screen and icons to tabs

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { Navigation } from 'react-native-navigation'
 import AuthScreen from './src/screens/Auth/Auth'
+import SharePlaceScreen from './src/screens/SharePlace/SharePlace'
+import FindPlaceScreen from './src/screens/FindPlace/FindPlace'
 
 // Register Screens
 Navigation.registerComponent('scout-x.AuthScreen', () => AuthScreen)
+Navigation.registerComponent('scout-x.SharePlaceScreen', () => SharePlaceScreen)
+Navigation.registerComponent('scout-x.FindPlaceScreen', () => FindPlaceScreen)
 
 // Start App
 Navigation.startSingleScreenApp({
---
+++ src/screens/MainTabs/startMainTabs.js
@@ -0,0 +1,29 @@
+import { Navigation } from 'react-native-navigation'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+const startTabs = () => {
+    Promise.all([
+        Icon.getImageSource('md-map', 30),
+        Icon.getImageSource('md-share-alt', 30)
+    ])
+        .then(sources => {
+        Navigation.startTabBasedApp({
+            tabs: [
+                {
+                    screen: 'scout-x.FindPlaceScreen',
+                    label: 'Find Place',
+                    title: 'Find Place',
+                    icon: sources[0]
+                },
+                {
+                    screen: 'scout-x.SharePlaceScreen',
+                    label: 'Share Place',
+                    title: 'Share Place',
+                    icon: sources[1]
+                }
+            ]
+        })
+    })
+}
+
+export default startTabs;
