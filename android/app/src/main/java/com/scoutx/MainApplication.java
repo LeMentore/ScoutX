7b2af660a6ab7fe03dbc15c2b36540b4644483e8
Charlie Frye <klayton.alan@gmail.com>
2018-04-09 17:32:49 +0500

    rendering a screen (test)

--- android/app/src/main/java/com/scoutx/MainApplication.java
+++ android/app/src/main/java/com/scoutx/MainApplication.java
@@ -8,41 +8,27 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativenavigation.NavigationApplication;
+
 import java.util.Arrays;
 import java.util.List;
 
-import com.oblador.vectoricons.VectorIconsPackage;
+public class MainApplication extends NavigationApplication {
 
-public class MainApplication extends Application implements ReactApplication {
+@Override
+     public boolean isDebug() {
+         return BuildConfig.DEBUG;
+     }
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+     protected List<ReactPackage> getPackages() {
+         return Arrays.<ReactPackage>asList(
+             new VectorIconsPackage()
+         );
+     }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new VectorIconsPackage()
-      );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+     @Override
+     public List<ReactPackage> createAdditionalReactPackages() {
+         return getPackages();
+     }
 }
