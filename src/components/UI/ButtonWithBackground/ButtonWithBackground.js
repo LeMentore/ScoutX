71802a3195715c1d57a23523e12ba3aa018346c7
Charlie Frye <klayton.alan@gmail.com>
2018-04-10 17:21:40 +0500

    buttonWithBackground creating ui component

---
+++ src/components/UI/ButtonWithBackground/ButtonWithBackground.js
@@ -0,0 +1,23 @@
+import React from  'react'
+import { TouchableOpacity, Text, View, StyleSheet } from 'react-native'
+
+const buttonWithBackground = props => (
+    <TouchableOpacity onPress={props.onPress}>
+        <View style={[styles.button, {backgroundColor: props.color}]}>
+            <Text style={styles.text}>{props.children}</Text>
+        </View>
+    </TouchableOpacity>
+)
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 10,
+        margin: 5,
+        borderRadius: 5,
+    },
+    text: {
+        color: '#fff'
+    }
+})
+
+export default buttonWithBackground
