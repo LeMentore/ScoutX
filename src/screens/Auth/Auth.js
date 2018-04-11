504a643b4484e637f0e0e2145260f259ee5d26a4
Charlie Frye <klayton.alan@gmail.com>
2018-04-11 15:20:57 +0500

    responsive auth

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, ImageBackground } from 'react-native'
+import { View, StyleSheet, ImageBackground, Dimensions } from 'react-native'
 
 import startMainTabs from '../MainTabs/startMainTabs'
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput'
@@ -15,17 +15,25 @@ class AuthScreen extends Component {
     }
 
     render() {
+        let headingText = null
+        if(Dimensions.get('window').height > 500){
+            headingText = (
+                <MainText>
+                    <HeadingText>Please Log In</HeadingText>
+                </MainText>
+            )
+        }
         return (
             <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
                 <View style={styles.container}>
-                    <MainText>
-                        <HeadingText>Please Log In</HeadingText>
-                    </MainText>
+                    {headingText}
                     <ButtonWithBackground color="#29aaf4" onPress={() => alert('Ты пидор!')}>Switch To Login</ButtonWithBackground>
                     <View style={styles.inputContainer}>
                         <DefaultInput placeholder="Your Email" style={styles.input}/>
-                        <DefaultInput placeholder="Password" style={styles.input}/>
-                        <DefaultInput placeholder="Confirm Password" style={styles.input}/>
+                        <View style={styles.passwordContainer}>
+                            <View style={styles.passwordWrapper}><DefaultInput placeholder="Password" style={styles.input}/></View>
+                            <View style={styles.passwordWrapper}><DefaultInput placeholder="Confirm Password" style={styles.input}/></View>
+                        </View>
                     </View>
                     <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>Submit</ButtonWithBackground>
                 </View>
@@ -50,6 +58,13 @@ const styles = StyleSheet.create({
     input: {
         backgroundColor: '#eee',
         borderColor: '#bbb'
+    },
+    passwordWrapper: {
+        width: Dimensions.get('window').height > 500 ? '100%' : '48%'
+    },
+    passwordContainer: {
+        flexDirection: Dimensions.get('window').height > 500 ? 'column': 'row',
+        justifyContent: 'space-between'
     }
 })
 
