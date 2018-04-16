39abb2bf474e34de698708aa18a8533241ffd13e
Charlie Frye <klayton.alan@gmail.com>
2018-04-16 16:54:23 +0500

    storing data in firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,19 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes'
 
 export const addPlace = (placeName, location, image) => {
-    return {
-        type: ADD_PLACE,
-        placeName: placeName,
+    const placeData = {
+        name: placeName,
         location: location,
         image: image
+    }
+    return dispatch => {
+        fetch('https://scoutx-1523612790305.firebaseio.com/places.json', {
+            method: 'POST',
+            body: JSON.stringify(placeData)
+        })
+            .catch(error => console.log(error))
+            .then(response => response.json())
+            .then(parsedResponse => console.log(parsedResponse))
     }
 }
 
