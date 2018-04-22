9d88ab0dff2db367891454abf62ca139b1a1bf2f
Charlie Frye <klayton.alan@gmail.com>
2018-04-22 17:39:44 +0500

    storing the remaining data

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,11 +2,6 @@ import { ADD_PLACE, DELETE_PLACE } from './actionTypes'
 
 export const addPlace = (placeName, location, image) => {
     return dispatch => {
-        const placeData = {
-            name: placeName,
-            location: location,
-            image: image
-        }
         fetch('https://us-central1-scoutx-1523612790305.cloudfunctions.net/storeImage', {
             method: 'POST',
             body: JSON.stringify({
@@ -16,15 +11,19 @@ export const addPlace = (placeName, location, image) => {
             .catch(error => console.log(error))
             .then(response => response.json())
             .then(parsedResponse => {
-                console.log(parsedResponse)
+                const placeData = {
+                    name: placeName,
+                    location: location,
+                    image: parsedResponse.imageUrl
+                }
+                return fetch('https://scoutx-1523612790305.firebaseio.com/places.json', {
+                    method: 'POST',
+                    body: JSON.stringify(placeData)
+                })
             })
-        // fetch('https://scoutx-1523612790305.firebaseio.com/places.json', {
-        //     method: 'POST',
-        //     body: JSON.stringify(placeData)
-        // })
-        //     .catch(error => console.log(error))
-        //     .then(response => response.json())
-        //     .then(parsedResponse => console.log(parsedResponse))
+            .catch(error => console.log(error))
+            .then(response => response.json())
+            .then(parsedResponse => console.log(parsedResponse))
     }
 }
 
