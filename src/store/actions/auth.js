7eff077b006a7211525810da75a348910db2be5c
Charlie Frye <klayton.alan@gmail.com>
2018-04-26 22:57:58 +0500

    signing users up

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,31 @@
 import { TRY_AUTH } from './actionTypes'
 
 export const tryAuth = authData => {
-    return {
-        type: TRY_AUTH,
-        authData: authData
+    return dispatch => {
+        dispatch(authSignUp(authData))
     }
 }
+
+export const authSignUp = (authData => {
+    return dispatch => {
+        fetch('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA0594HwEhXIp2M7nsKYDfS1VjvTRocJeE', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .catch(error => {
+                console.log(error)
+                alert('Auth failed. Try Again...')
+            })
+            .then(response => response.json())
+            .then(parsedResponse => {
+                console.log(parsedResponse)
+            })
+    }
+})
