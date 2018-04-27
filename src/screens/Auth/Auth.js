bd49bf99477148c4f20e4208d0882b5892ad146a
Charlie Frye <klayton.alan@gmail.com>
2018-04-27 21:15:17 +0500

    using auth result

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, ImageBackground, Dimensions, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback } from 'react-native'
+import { View, StyleSheet, ImageBackground, Dimensions, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback, ActivityIndicator } from 'react-native'
 import { connect } from 'react-redux'
 
-import startMainTabs from '../MainTabs/startMainTabs'
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput'
 import HeadingText from '../../components/UI/HeadingText/HeadingText'
 import MainText from '../../components/UI/MainText/MainText'
@@ -72,7 +71,6 @@ class AuthScreen extends Component {
             password: this.state.controls.password.value
         }
         this.props.onLogin(authData)
-        startMainTabs()
     }
 
     updateInputState = (key, value) => {
@@ -116,6 +114,17 @@ class AuthScreen extends Component {
     render() {
         let headingText = null
         let confirmPasswordControl = null
+        let submitButton = (
+            <ButtonWithBackground color="#29aaf4"
+                                  onPress={this.loginHandler}
+                                  disabled={
+                                      !this.state.controls.confirmPassword.valid && this.state.authMode === 'signup' ||
+                                      !this.state.controls.email.valid ||
+                                      !this.state.controls.password.valid
+                                  }>
+                Submit
+            </ButtonWithBackground>
+        )
 
         if(this.state.viewMode === 'portrait'){
             headingText = (
@@ -136,6 +145,9 @@ class AuthScreen extends Component {
                     />
                 </View>
             )
+        }
+        if(this.props.isLoading){
+            submitButton = <ActivityIndicator />
         }
         return (
             <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
@@ -176,16 +188,7 @@ class AuthScreen extends Component {
                             </View>
                         </View>
                     </TouchableWithoutFeedback>
-
-                    <ButtonWithBackground color="#29aaf4"
-                                          onPress={this.loginHandler}
-                                          disabled={
-                                              !this.state.controls.confirmPassword.valid && this.state.authMode === 'signup' ||
-                                              !this.state.controls.email.valid ||
-                                              !this.state.controls.password.valid
-                                          }>
-                        Submit
-                    </ButtonWithBackground>
+                    {submitButton}
                 </KeyboardAvoidingView>
             </ImageBackground>
         )
@@ -225,10 +228,16 @@ const styles = StyleSheet.create({
     }
 })
 
+const mapStateToProps = state => {
+    return {
+        isLoading: state.ui.isLoading
+    }
+}
+
 const mapDispatchToProps = dispatch => {
     return {
         onLogin: authData => dispatch(tryAuth(authData))
     }
 }
 
-export default connect(null, mapDispatchToProps)(AuthScreen)
+export default connect(mapStateToProps, mapDispatchToProps)(AuthScreen)
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,6 @@
 import { TRY_AUTH } from './actionTypes'
+import { uiStartLoading, uiCompleteLoading } from './ui'
+import startMainTabs from '../../screens/MainTabs/startMainTabs'
 
 export const tryAuth = authData => {
     return dispatch => {
@@ -8,6 +10,7 @@ export const tryAuth = authData => {
 
 export const authSignUp = (authData => {
     return dispatch => {
+        dispatch(uiStartLoading())
         fetch('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA0594HwEhXIp2M7nsKYDfS1VjvTRocJeE', {
             method: 'POST',
             body: JSON.stringify({
@@ -22,9 +25,16 @@ export const authSignUp = (authData => {
             .catch(error => {
                 console.log(error)
                 alert('Auth failed. Try Again...')
+                dispatch(uiCompleteLoading())
             })
             .then(response => response.json())
             .then(parsedResponse => {
+                dispatch(uiCompleteLoading())
+                if(parsedResponse.error){
+                    alert('Auth failed. Try Again...')
+                } else {
+                    startMainTabs()
+                }
                 console.log(parsedResponse)
             })
     }
