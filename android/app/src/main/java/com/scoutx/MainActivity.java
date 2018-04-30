5ac33e23a0a44e0cb0fa5cd560bc0c01ff0a86da
Charlie Frye <klayton.alan@gmail.com>
2018-04-30 20:44:19 +0500

    added splash screen

--- android/app/src/main/java/com/scoutx/MainActivity.java
+++ android/app/src/main/java/com/scoutx/MainActivity.java
@@ -1,7 +1,28 @@
 package com.scoutx;
 
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.util.TypedValue;
+
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
+    @Override
+    public LinearLayout createSplashLayout(){
+        LinearLayout view = new LinearLayout(this);
+        TextView textView = new TextView(this);
 
+        view.setBackgroundColor(Color.parseColor("#58595b"));
+        view.setGravity(Gravity.CENTER);
+
+        textView.setTextColor(Color.parseColor("#d9da24"));
+        textView.setText("ScoutX");
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+        view.addView(textView);
+        return view;
+    }
 }
